852a84f59c57d32a01b1ed157b023ef16d8a2ec0
David091214 <unfsams909@gmail.com>
2026-02-12 01:24:20 +0800

    first_commit
    using the idea of loop to do some simple tasks

---
+++ loop.py
@@ -0,0 +1,11 @@
+ # a simple variable to hold list of items 
+My_List = ["apple", "orange", "fish"]
+print(My_List)
+
+ # append to the list of items 
+My_List.append("fruits")
+
+# using item as an iteratoe varianle
+
+for item in My_List:
+    print(item)
