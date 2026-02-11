1d807a71d7317d21c1ca97d39deefafcf4af6c43
David091214 <unfsams909@gmail.com>
2026-02-12 00:43:28 +0800

    Create hello.py
    explanation of print and algebrain expressions

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("hello world ")
+
+print(type(3))
+
+
+# this basically divided 5 by 2 twice and since 
+# and since 2 cannot go into 5 3 times it outputs 
+# the value 2 
+print(5 // 2 )  
+
+# in respect to the above solving if you
+# want to see the remainder we use modulus
+
+print(5%2)
+
