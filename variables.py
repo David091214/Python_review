1db50bcaf333e6b475eaa4d15f7c098e9e6bd888
David091214 <unfsams909@gmail.com>
2026-02-12 00:55:31 +0800

    first_commit
    explaining some variable in python

---
+++ variables.py
@@ -0,0 +1,11 @@
+# here y is a variable 
+# called a variable because it varies because 
+# the value can be changed and the data type can change
+
+y = 5
+print ( y )
+
+#boolean is also a variable 
+# can either be True or False 
+
+print (4<3)
