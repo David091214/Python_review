24fa6a655081c5885886421061c3ffd79336a78a
David091214 <unfsams909@gmail.com>
2026-02-12 01:08:00 +0800

    Update variables.py

--- variables.py
+++ variables.py
@@ -8,4 +8,33 @@ print ( y )
 #boolean is also a variable 
 # can either be True or False 
 
-print (4<3)
+
+print (4<3) # < is the comparison operator
+
+# others are >= , <= , != , == 
+# can also be used for strings
+print ("hi" == "hi")
+
+# also 
+
+if False:
+    print("h") #won't be printed be it's false
+
+if True:
+    print("hh") # will be printed because it is true
+
+
+# example
+
+y = 5 
+
+z = y > 3
+
+if z: # here z is a variable saying True 
+    print ("the y must be greater than 3")
+
+
+# example 2
+
+if 4 > 3:
+    print ("example 2")
